9c47b7308e840237f65578ae5a51e5b0dd50263f
seyeala <se.alaie3141@gmail.com>
2024-05-07 16:08:22 -0600

    m

--- test/new.py
+++ test/new.py
@@ -44,7 +44,7 @@ print('inputT')
 input_ids = inputT['input_ids'].to(device)
 
 print(input_ids)
-outT=model(input_ids, max_length=2)
+outT=model(input_ids)
 last_hidden_state = outT.last_hidden_state
 
 print(last_hidden_state)
