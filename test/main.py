4bf4d090a665c508b78d6d09a9da4438b2a87c13
seyeala <se.alaie3141@gmail.com>
2024-05-07 20:33:05 -0600

    m

--- test/main.py
+++ test/main.py
@@ -8,6 +8,8 @@ from torch.utils.data import DataLoader, TensorDataset
 from torch.optim import Adam
 
 # Assuming NumericLMWrapper is imported and configured as above
+model_name = "openai-community/gpt2-large"  # substitute with the actual model you are using
+
 numeric_lm = NumericLMWrapper(model_name, project_input=True, project_output=True, device='cuda')
 numeric_lm.train()  # Set the model to training mode
 
