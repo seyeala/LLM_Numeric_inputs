d18780efc596a319c3adb04830cc5505ce3423f7
seyeala <se.alaie3141@gmail.com>
2024-05-09 16:11:46 -0600

    C

--- alignmentTextN.py
+++ alignmentTextN.py
@@ -76,10 +76,11 @@ if __name__ == "__main__":
         config = yaml.safe_load(file)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    # Create the model instance
     llm = NumericLMWrapper(config['model_name'], project_input=False, project_output=True, device=device)
-
-    # Configure which parts of the model should be trainable
     llm.configure_trainable_layers(train_input_projection=False, train_output_projection=True, train_transformer=False)
 
-    alignment(llm, config, config['num_epochs'], args.model_path, args.savestage2, config['shl'])
+    # Ensure the tokenizer has a pad token set
+    if llm.tokenizer.pad_token is None:
+        llm.tokenizer.pad_token = llm.tokenizer.eos_token
+
+    alignment(llm, config, config['num_epochs'], args.model_path, args.save_model_path, config['shl'])
